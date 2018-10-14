a69b93441e1f460fa13128e7982f974479f7e03e
Ben Hurst <BHurst8@gmail.com>
2018-10-14 15:57:41 -0700

    Integrated custom login/logout

--- book_share_project/urls.py
+++ book_share_project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view
+from .views import home_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('dashboard/', home_view, name='home'),
+    path('accounts/logout', logout_view, name='logout'),
+    path('', home_view, name='home'),
     path('books/', include('book_share_app.urls'))
 ]
--- book_share_project/views.py
+++ book_share_project/views.py
@@ -3,36 +3,41 @@ from django.core.exceptions import PermissionDenied
 from book_share_app.models import Profile
 from allauth.socialaccount.models import SocialAccount
 import requests
-import json
 
 
 def home_view(request):
+    if request.user.is_authenticated:
+        profile = Profile.objects.filter(user_id__username=request.user.username)
+
+        fb_account = SocialAccount.objects.filter(user_id__username=request.user.username)
+
+        # We have the right social_account instance (i.e., table row). There has to be an easier way to grab the uid (i.e., the cell in that row)
+        uid = list(fb_account.values('uid'))[0]['uid']
+        extra_data = list(fb_account.values('extra_data'))[0]['extra_data']
+
+        if not profile:
+
+            endpoint = 'https://graph.facebook.com/{}?fields=picture'.format(uid)
+            headers = {"Authorization": "Bearer 278758722745488|zgMN8ZoPRELH14yZO8TiQQNGgPM"}
+            response = requests.get(endpoint, headers=headers).json()
+            picture = response['picture']['data']['url']
+
+            Profile.objects.create(
+                user_id=request.user,
+                username=request.user.username,
+                email=request.user.email,
+                first_name=request.user.first_name,
+                last_name=request.user.last_name,
+                fb_id=uid,
+                picture=picture,
+            )
+
+    return render(request, 'base/home.html')
+
+
+def logout_view(request):
     if not request.user.is_authenticated:
         raise PermissionDenied
 
-    profile = Profile.objects.filter(user_id__username=request.user.username)
 
-    fb_account = SocialAccount.objects.filter(user_id__username=request.user.username)
-
-    # We have the right social_account instance (i.e., table row). There has to be an easier way to grab the uid (i.e., the cell in that row)
-    uid = list(fb_account.values('uid'))[0]['uid']
-    extra_data = list(fb_account.values('extra_data'))[0]['extra_data']
-
-    if not profile:
-
-        endpoint = 'https://graph.facebook.com/{}?fields=picture'.format(uid)
-        headers = {"Authorization": "Bearer 278758722745488|zgMN8ZoPRELH14yZO8TiQQNGgPM"}
-        response = requests.get(endpoint, headers=headers).json()
-        picture = response['picture']['data']['url']
-
-        Profile.objects.create(
-            user_id=request.user,
-            username=request.user.username,
-            email=request.user.email,
-            first_name=request.user.first_name,
-            last_name=request.user.last_name,
-            fb_id=uid,
-            picture=picture,
-        )
-
-    return render(request, 'base/home.html')
+    return render(request, 'custom_account/logout.html')
